2d6c5eb143f50634dda08330726521368dffe758
Thomas Butler <2511516+fnrhombus@users.noreply.github.com>
2026-08-15 03:18:02 -0400

    feat(func): thread a This type parameter through the function family

--- libraries/func/src/index.d.ts
+++ libraries/func/src/index.d.ts
@@ -1,24 +1,31 @@
-
-
-
-export type Func<in Args extends readonly any[] = any[], out Return = any> = (...args: Args) => Return;
-export type AsyncFunc<in Args extends readonly any[] = any[], out Return = any> = Func<Args, Promise<Awaited<Return>>>;
+export type Func<in Args extends readonly any[] = any[], out Return = any, in This = unknown> = (
+    this: This,
+    ...args: Args
+) => Return;
+export type AsyncFunc<in Args extends readonly any[] = any[], out Return = any, in This = unknown> = Func<
+    Args,
+    Promise<Awaited<Return>>,
+    This
+>;
 // export type AsyncFunc<Args extends any[] = any[], out Return = any> = Func<Args, Promise<Return>>;
 
-export type Action<in Args extends readonly any[] = any[]> = Func<Args, void>;
-export type AsyncAction<in Args extends readonly any[] = any[]> = AsyncFunc<Args, void>;
+export type Action<in Args extends readonly any[] = any[], in This = unknown> = Func<Args, void, This>;
+export type AsyncAction<in Args extends readonly any[] = any[], in This = unknown> = AsyncFunc<Args, void, This>;
 
-export type Sub<in Args extends readonly any[] = any[]> = Action<Args>;
-export type AsyncSub<in Args extends readonly any[] = any[]> = AsyncAction<Args>;
+export type Sub<in Args extends readonly any[] = any[], in This = unknown> = Action<Args, This>;
+export type AsyncSub<in Args extends readonly any[] = any[], in This = unknown> = AsyncAction<Args, This>;
 
 export interface Ctor<in Args extends readonly any[] = any[], out Instance = any> {
-  new(...args: Args): Instance;
-  prototype: Instance;
+    new (...args: Args): Instance;
+    prototype: Instance;
 }
 
-type _AbstractCtor<in Args extends readonly any[] = any[], out Instance = any> = abstract new (...args: Args) => Instance;
-export interface AbstractCtor<in Args extends readonly any[] = any[], out Instance = any> extends _AbstractCtor<Args, Instance> {
-  prototype: Instance;
+type _AbstractCtor<in Args extends readonly any[] = any[], out Instance = any> = abstract new (
+    ...args: Args
+) => Instance;
+export interface AbstractCtor<in Args extends readonly any[] = any[], out Instance = any> extends _AbstractCtor<
+    Args,
+    Instance
+> {
+    prototype: Instance;
 }
-
-
